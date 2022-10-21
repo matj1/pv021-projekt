f1c008c9d822d2df93e88efbc71d9d5de3b6fc28
Martin <492978@mail.muni.cz>
2022-10-21 21:26:31 +0200

    konečně to funguje !!!!

--- src/neuronka.c
+++ src/neuronka.c
@@ -8,9 +8,9 @@
 #define VSTUPU 784
 #define VYSTUPU 10
 #define SKLON 0.005
-#define RYCHLOST 0.005
+#define RYCHLOST 0.01 //když je moc velká, tak celá neuronka spadne do předpovídání jediné kategorie
 #define DELKA_UCENI 5000
-#define BEC 1
+#define BEC 10
 
 
 // za sebe mluvící různé pokusné aktivační funkce, použije se nejspíš relu
@@ -23,14 +23,14 @@ float relu(float x) {
 	if (x > 0) {
 		return x;
 	}
-	return 0; // SKLON * x;
+	return 0; // SKLON * x; //?
 }
 
 float der_relu(float x) {
 	if (x > 0) {
 		return 1;
 	}
-	return SKLON; // 0;
+	return SKLON; // 0; //?
 }
 
 /*
@@ -50,99 +50,80 @@ int *pocty počet neuronů v každé vrstve bez thresholdů
 */
 
 int iterace(float ***vaha, float **vysledky, float **neu, int vrstvy, int *pocty) {
-	//printf("iterace\n"); // debug
 	for (int i = 0; i < vrstvy;
 	     ++i) { // vrstev je vrstvy +2 (posledni index je vrstvy+1), ale posledni je výstupni
-		// printf("%d. vrstva, %d neuronu v nizsi, %d neuronu ve vyssi\n", i, pocty[i], pocty[i + 1]); // debug
 		vysledky[i][pocty[i]] = 1;                // nastaveni thresholdového neuronu
 		for (int k = 0; k < pocty[i + 1]; ++k) {  // pro každý neuron vyšši vrstvy
 			neu[i + 1][k] = 0;                  // vynulovani na začatku sumovani
-			// printf("neuron %d vynulovan\n", k); // debug
 			for (int j = 0; j < pocty[i] + 1;
 			     ++j) { // pro každý neuron aktualni vrstvy +1 aby se počitalo i s thresholdem
-				// printf("pricita se %d. neuron\n",j);
-				// printf("spodni %f\n",vysledky[i][j]);
-				// printf("%d, %d, %d\n",i,j,k);
-				// printf("vaha %f\n",vaha[i][j][k]);
 				neu[i + 1][k] += vysledky[i][j] * vaha[i][j][k];
 			}
-			//printf("suma %d neuronu %d vrstvy je: %f\n",k, i+1, neu[i + 1][k]);
 			vysledky[i + 1][k] = relu(neu[i + 1][k]); // otazka vice aktivacnich funkci
 		}
 	}
 
 	//řešeni výstupni vrstvy
-	int max = 0;
+	int max = 7;
+	neu[vrstvy + 1][7] = 0;
+	vysledky[vrstvy][pocty[vrstvy]] = 1; // nastaveni thresholdu
 	for (int i = 0; i < VYSTUPU; ++i) {
 		neu[vrstvy + 1][i] = 0;
-		vysledky[vrstvy][pocty[vrstvy]] = 1; // nastaveni thresholdu
 		for (int j = 0; j < pocty[vrstvy] + 1; ++j) {
 			neu[vrstvy + 1][i] += vysledky[vrstvy][j] * vaha[vrstvy][j][i];
 		}
-		//printf("suma %d neuronu %d vrstvy je: %f\n",i, vrstvy + 1, neu[vrstvy + 1][i]);
 		if (neu[vrstvy + 1][i] > neu[vrstvy+1][max]) { //odecitani kvuli preteceni
 			max = i;
 		}
 	}
+
 	float suma = 0;
 	for (int i = 0; i < VYSTUPU; ++i) {
 		suma += exp(neu[vrstvy + 1][i]- neu[vrstvy+1][max]); //odecitani kvuli preteceni
 	}
 	for (int i = 0; i < VYSTUPU; ++i) {
 		vysledky[vrstvy + 1][i] = exp(neu[vrstvy + 1][i] - neu[vrstvy + 1][max]) / suma; //odecitani kvuli preteceni
-		//printf("vysledek %d neuronu: %f\n",i,vysledky[vrstvy + 1][i]); //debug
 	}
 	return max;
 }
 
 int bekpropagejsn(float ***vaha, float **neu, float **vysledky, float **derivace, int vrstvy, int *pocty, int cil, int **odhady) {
-	//printf("trenuju\n"); // debug
 	int vysledek = iterace(vaha, vysledky, neu, vrstvy, pocty);
-	//printf("vysledky: %d %d\n",cil, vysledek); //debug
 	odhady[cil][vysledek]+=1;
-	//printf("hototvo\n"); //debug
-	//printf("po iteraci, vysledek= %d\n", vysledek); // debug
 	for (int i = 0; i < VYSTUPU; ++i) {
 		if (i != cil) {
-			derivace[vrstvy + 1][i] += vysledky[vrstvy + 1][i]; 
-			      // anebo += , připrava na přistup nekolik přikladů: jedno učeni
-			//printf("derivace %d neuronu 3. vrstvy: %.2f\n", i, derivace[vrstvy + 1][i]); //debug
+			derivace[vrstvy + 1][i] = vysledky[vrstvy + 1][i];
 		}
 	}
-	derivace[vrstvy + 1][cil] += vysledky[vrstvy + 1][cil] - 1 ; // taky nebo +=
-	//printf("derivace %d neuronu 3. vrstvy: %.2f\n", cil, derivace[vrstvy + 1][cil]); //debug
-
+	derivace[vrstvy + 1][cil] = vysledky[vrstvy + 1][cil] - 1;
 	for (int i = vrstvy; i > 0; --i) {
 		for (int j = 0; j < pocty[i]; ++j) {
-			// derivace[i][j] = 0; // přesunout do nadřazené funkce ?
+			derivace[i][j] = 0;
 			for (int k = 0; k < pocty[i + 1]; ++k) {
 				derivace[i][j] += vaha[i][j][k] * der_relu(neu[i][j]) * derivace[i + 1][k];
 			}
-			//printf("derivace %d neuronu %d vrstvy: %.4f\n", j,i, derivace[i][j]); //debug
 		}
 	}
 	return vysledek == cil; // můžeme potom sečist výsledky tréninku pro celkový počet spravne odhadnutých čislic
 }
 
-int trenink(float *priklady, int pozice, float ***vaha, float **neu, float **vysledky, float **derivace, int vrstvy, int *pocty, int *cile, int **odhady) {
+int trenink(float *priklady, int pozice, float ***vaha, float **neu, float ***vysledky, float ***derivace, int vrstvy, int *pocty, int *cile, int **odhady) {
 	int spravne=0;
-	for (int i=1;i< vrstvy+2; ++i){
-		memset(derivace[i],0,pocty[i]*sizeof(float));
-	}
-	
+
 	//TODO tohle je tvoje parketa na paralelizaci, už se na ni těším
-	
+
 	for (int i=0; i<BEC; ++i){
-		//printf("%d \n",i); //debug
-		vysledky[0] = &priklady[(pozice + i) * (VSTUPU+1)];
-		//printf("po prirazeni %d\n",i); //debug
-		spravne+=bekpropagejsn(vaha, neu, vysledky, derivace, vrstvy, pocty, cile[i], odhady);
+		vysledky[i][0] = &priklady[(pozice + i) * (VSTUPU+1)];
+		spravne+=bekpropagejsn(vaha, neu, vysledky[i], derivace[i], vrstvy, pocty, cile[pozice+i], odhady);
 	}
-	
-	for (int i = 0; i < vrstvy + 1; ++i) {
-		for (int j = 0; j < pocty[i] + 1; ++j) {
-			for (int k = 0; k < pocty[i + 1]; ++k) {
-				vaha[i][j][k] -= derivace[i + 1][k] * vysledky[i][j] * RYCHLOST; // RYCHLOST jako funkce nečeho ?
+	//změna vah
+	for (int i=0; i< BEC; ++i){
+		//printf("%d beč\n",i);
+		for (int j = 0; j < vrstvy+1 ; ++j) {
+			for (int l = 0; l < pocty[j + 1]; ++l) {
+				for (int k = 0; k < pocty[j] + 1; ++k) {
+					vaha[j][k][l] -= derivace[i][j + 1][l] * vysledky[i][j][k] * RYCHLOST; // RYCHLOST jako funkce nečeho ?
+				}
 			}
 		}
 	}
@@ -166,12 +147,8 @@ int main(int argc, char **argv) {
 		printf("%d\n", pocty[i]);
 	}
 	printf("%d\n", pocty[vrstvy + 1]);
-
 	float **vaha[vrstvy + 1];
-
-	printf("vahy\n"); // debug
 	srand(2);
-
 	for (int j = 0; j < vrstvy + 1; ++j) {
 		vaha[j] = malloc((pocty[j] + 1) * sizeof(float *));
 		for (int k = 0; k < pocty[j] + 1; ++k) {
@@ -179,96 +156,109 @@ int main(int argc, char **argv) {
 			for (int g = 0; g < pocty[j + 1]; ++g) {
 				vaha[j][k][g] = 0.1 * (float)rand() / (float)RAND_MAX - 0.05; //
 				                   // inicializace vah
-				// printf("%f\n",vaha[j][k][g]); //debug
 			}
 		}
 	}
-	//printf("%f\n", vaha[0][0][0]); // debug
-	//printf("neurony\n");           // debug
 	float *neu[vrstvy + 2];        // neu[0] ale nepouživam (přehlednost ?)
-	float *vysledky[vrstvy + 2];
-	float *derivace[vrstvy + 2]; // derivace[0] taky nepouživam
-
-	// neu[0]= float neurony[VSTUPU];
-	// vysledky[0] = float vysledek[VSTUPU];
-
+	float **vysledky[BEC];
+	float **derivace[BEC];
 	for (int j = 1; j < vrstvy + 2; ++j) {
 		neu[j] = malloc(pocty[j] *
 		                sizeof(float)); // neu[j-1] ? neni potřeba sumovat vstupni vrstvu
-		vysledky[j] = malloc((pocty[j] + 1) * sizeof(float));
-		derivace[j] =
-		      malloc(pocty[j] *
-		             sizeof(float)); // nebo der[j-1] ? nepouzivam derivaci prvni vstupni vrstvy
 	}
-	//printf("%f\n", derivace[1][2]); // debug
-
-	/*
-	if (argc > i) {
-	  FILE *koef;
-	  koef = fopen(argv[i], "r");
+	for (int k=0; k<BEC; ++k){
+		vysledky[k]=malloc((vrstvy+2)*sizeof(float *));
+		for (int i=0; i<vrstvy+2; ++i){
+			vysledky[k][i] = malloc((pocty[i] + 1) * sizeof(float)); //vim, ze neni potreba alokovat threshold vystupni vrstvy, ale co uz
+		}
 	}
-	*/
 
+	for (int k=0; k<BEC; ++k){
+		derivace[k]=malloc((vrstvy+2)*sizeof(float *));
+		for (int i=0; i<vrstvy+2; ++i){
+			derivace[k][i] = malloc(pocty[i] * sizeof(float));
+		}
+	}
 	// načteni dat a trénink
-
-	printf("hehe\n");
 	// TODO pořešit cesty k datům
-	FILE *vstup = fopen("../../data/fashion_mnist_train_vectors.csv", "r");
+	FILE *vstup = fopen("../../data/train_vectors.csv", "r");
+	FILE *validacni_vektor = fopen("../../data/val_train_vectors.csv", "r");
+	
 	printf("vstupy ok\n");
-	FILE *vystupy = fopen("../../data/fashion_mnist_train_labels.csv", "r");
+	FILE *vystupy = fopen("../../data/train_labels.csv", "r");
+	FILE *validacni_popisek = fopen("../../data/val_train_labels.csv", "r");
+
 	printf("vystupy ok\n");
 	int delka = secti_radky(vystupy);
+	int val_delka = secti_radky(validacni_popisek);
+
 	float *priklady = nacist_data(vstup, delka);
+	float *val_priklady = nacist_data(validacni_vektor, val_delka);
+
 	printf("nacitam cile\n");
 	int *cile = nacist_cile(vystupy, delka);
+	int *val_cile = nacist_cile(validacni_popisek, val_delka);
+	
 	int spravne;
 	int *odhady[10];
-	
-	/* // debug
-	for (int i=59999*785+1; i< 60000*785; ++i){
-	printf("%f, ",priklady[i]);
-	}
-	printf("\n");
-	printf("%d\n",cile[59999]);
-	*/
+
 	FILE *ven=fopen("vahy2.txt","w");
 	printf("zacatek treninku\n");
+	for (int i=0;i<10;++i){
+		odhady[i]=malloc(10*sizeof(int));
+		for (int j=0;j<10;++j){
+			odhady[i][j]=0;
+		}
+	}
+
 	for (int p = 0; p < 50; ++p) {
 		for (int i=0;i<10;++i){
-			odhady[i]=malloc(10*sizeof(int));
-			for (int j=0;j<10;++j){
-				odhady[i][j]=0;
-			}
+			memset(odhady[i],0,10*sizeof(int));
 		}
-		//printf("%d. kolo\n", p); //debug
+
 		spravne=0;
-		for (int i = 0; i < delka; i+=BEC) {
-			//vysledky[0] = &priklady[i * (VSTUPU+1)];
-			/* //debug
-			for (int j=0; j<VSTUPU;++j){
-			      printf("%.0f, ",vysledky[0][j]);
-			}
-			printf("\n");
-			*/
-			//vypis_vahy(ven, vaha,pocty, vrstvy); //debug
-			//printf("pred %d\n",i); //debug
-			spravne += trenink(priklady, i, vaha, neu, vysledky, derivace, vrstvy, pocty, &cile[i], odhady);
-			//printf("po\n"); //debug
-			//fprintf(ven,"=====================================================\n");
+		for (int i = 0; i < delka -BEC +1; i+=BEC) {
+			spravne += trenink(priklady, i, vaha, neu, vysledky, derivace, vrstvy, pocty, cile, odhady);
 		}
+
 		for (int v=0; v<10;++v){
 			for (int w=0; w<10;++w){
 				printf("%8d",odhady[v][w]);
 			}
 			printf("\n");
 		}
-		// vypis_vahy(ven, vaha,pocty, vrstvy); //debug
 		printf("%d. kolo %d\n", p,spravne);
+
+		printf("validace\n");
+		spravne=0;
+
+		for (int i=0;i<10;++i){
+			memset(odhady[i],0,10*sizeof(int));
+		}
+		int vysledek;
+		for (int i=0; i< val_delka;++i){
+			vysledky[0][0]=&val_priklady[i*(VSTUPU+1)];
+			vysledek=iterace(vaha, vysledky[0], neu, vrstvy, pocty);
+			spravne+=val_cile[i]==vysledek;
+			odhady[val_cile[i]][vysledek]+=1;
+		}
+
+		for (int v=0; v<10;++v){
+			for (int w=0; w<10;++w){
+				printf("%8d",odhady[v][w]);
+			}
+			printf("\n");
+		}
+		printf("validace po %d. kole %d\n", p,spravne);
 	}
-	// vypis_vahy(ven, vaha,pocty, vrstvy);
+	vypis_vahy(ven, vaha,pocty, vrstvy);
 	free(priklady);
 	return 0;
 }
+/*
+příklad spuštění: ./projekt 2 40 15
+*/
+
 
 /* Přehled všeho, co ješte neni dořešené:
 1. Kde budou data relativne ke kódu
